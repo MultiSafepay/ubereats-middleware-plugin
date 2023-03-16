6659fd8e31b124f9727c38241e7a709d870d85f0
jrivasmsp <101558683+jrivasmsp@users.noreply.github.com>
2023-03-16 13:08:05 +0000

    wip

--- src/routes/ubereats.php
+++ src/routes/ubereats.php
@@ -11,15 +11,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('ubereats')->group(
     function () {
         Route::post('webhook', Webhook::class);
-
-        Route::middleware(['auth:sanctum'])->group(
-            function () {
-                Route::post('menu', Menu::class);
-                Route::post('delivery_tracking', DeliveryTracking::class);
-                Route::post('orders/accept', [Order::class, 'accept']);
-                Route::post('orders/deny', [Order::class, 'deny']);
-                Route::post('orders/cancel', [Order::class, 'cancel']);
-            }
-        );
+        Route::post('menu', Menu::class);
+        Route::post('delivery_tracking', DeliveryTracking::class);
+        Route::post('orders/accept', [Order::class, 'accept']);
+        Route::post('orders/deny', [Order::class, 'deny']);
+        Route::post('orders/cancel', [Order::class, 'cancel']);
     }
 );
